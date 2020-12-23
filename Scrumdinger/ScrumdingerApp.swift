7188c2444d646a1fa76f7daee90d7e36565b9d42
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-12-23 06:56:21 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ataias Pereira Reis on 23/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
