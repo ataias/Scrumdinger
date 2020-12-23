5a4bb1aeca67b21fabdf686b2658c51de7d12969
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-12-23 07:49:35 -0300

    Add card view with colors

---
+++ Scrumdinger/Models/Color+Codable.swift
@@ -0,0 +1,91 @@
+//
+//  Color+Codable.swift
+//  Scrumdinger
+//
+
+//Copyright © 2020 Apple Inc.
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import SwiftUI
+
+extension Color: Codable {
+    private struct Components {
+        let red: Double
+        let green: Double
+        let blue: Double
+        let alpha: Double
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case red
+        case green
+        case blue
+        case alpha
+    }
+
+    /// A new random color.
+    static var random: Color {
+        let red = Double.random(in: 0...1)
+        let green = Double.random(in: 0...1)
+        let blue = Double.random(in: 0...1)
+        return Color(.sRGB, red: red, green: green, blue: blue, opacity: 1)
+    }
+
+    private var components: Components {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        UIColor(self).getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        return Components(red: Double(red),
+                          green: Double(green),
+                          blue: Double(blue),
+                          alpha: Double(alpha))
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let red = try container.decode(Double.self, forKey: .red)
+        let green = try container.decode(Double.self, forKey: .green)
+        let blue = try container.decode(Double.self, forKey: .blue)
+        let alpha = try container.decode(Double.self, forKey: .alpha)
+        self.init(Components(red: red, green: green, blue: blue, alpha: alpha))
+    }
+
+    private init(_ components: Components) {
+        self.init(.sRGB, red: components.red, green: components.green, blue: components.blue, opacity: components.alpha)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        let components = self.components
+        try container.encode(components.red, forKey: .red)
+        try container.encode(components.green, forKey: .green)
+        try container.encode(components.blue, forKey: .blue)
+        try container.encode(components.alpha, forKey: .alpha)
+    }
+
+    // MARK: - font colors
+    /// This color is either black or white, whichever is more accessible when viewed against the scrum color.
+    var accessibleFontColor: Color {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        UIColor(self).getRed(&red, green: &green, blue: &blue, alpha: nil)
+        return isLightColor(red: red, green: green, blue: blue) ? .black : .white
+    }
+
+    private func isLightColor(red: CGFloat, green: CGFloat, blue: CGFloat) -> Bool {
+        let lightRed = red > 0.65
+        let lightGreen = green > 0.65
+        let lightBlue = blue > 0.65
+
+        let lightness = [lightRed, lightGreen, lightBlue].reduce(0) { $1 ? $0 + 1 : $0 }
+        return lightness >= 2
+    }
+}
