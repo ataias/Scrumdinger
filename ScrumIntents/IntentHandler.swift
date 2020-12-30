07134fcdd0b8a8cd7aad98e4bbfb1a8a95434ceb
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-12-30 14:28:47 -0300

    WIP: trying to get intent to be handled

--- ScrumIntents/IntentHandler.swift
+++ ScrumIntents/IntentHandler.swift
@@ -6,12 +6,22 @@
 //
 
 import Intents
+import SwiftUI
+
+public enum Constants {
+    static let storage = "demostorage"
+    static let group = "br.com.ataias.Scrumdinger"
+}
 
 class IntentHandler: INExtension {
+    @AppStorage(Constants.storage, store: UserDefaults(
+                    suiteName: Constants.group)) var store: String = "ORIGINAL"
+
     override func handler(for intent: INIntent) -> Any {
         // This is the default implementation.  If you want different objects to handle different intents,
         // you can override this and return the handler you want for that particular intent.
-        
+
+        store = "CALLED AT \(Date())"
         return ScrumIntentHandler()
     }
     
--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -33,3 +33,7 @@ struct ScrumdingerApp: App {
         }
     }
 }
+
+//@UIApplicationMain
+//class AppDelegate: UIResponder, UIApplicationDelegate {
+//    }
